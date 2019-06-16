b9590dcd2ccb3ff72be3245d387ec663fcd3ebf2
ScriptsEngineer <scriptsengineer@gmail.com>
2019-06-16 14:25:47 -0300

    Add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] [Tooltip("Run Speed to player movement")]
+    private float runSpeed;
+
+    private Rigidbody2D rigidBody2D;
+
+    private float horizontal;
+    private float vertical;
+
+    private void Awake() {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update ()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void FixedUpdate() {
+        rigidBody2D.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
